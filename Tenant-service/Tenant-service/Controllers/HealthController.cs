9e76e3bc617ba2664f952be81e11700090b1d574
Robin van Hoof <robbie@vanhoof-erp.nl>
2024-01-06 23:42:04 +0100

    Add Health Endpoint

---
+++ Tenant-service/Tenant-service/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Logging_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
